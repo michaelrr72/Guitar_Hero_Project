7349aef4e92994d9b468f827099a51ab3674fd01
Michael Rodriguez Rios <michael_rios05@hotmail.com>
2024-04-06 01:21:41 -0500

    Pruebas de movimiento

--- js/main.js
+++ js/main.js
@@ -1,17 +1,48 @@
 var contenedor = document.querySelector(".contenedor");
 var alturaContenedor = document.querySelector('.contenedor').offsetHeight;
 var pauseButton = document.querySelector('.play-pause');
+var teclas = ['A', 'S', 'D', 'J', 'K', 'L'];
 let pausado = false;
 
+/*
 function crearNota(i) {
+    console.log(`.columna[data-key="${i}"]`);
+    var contenedorColumna = document.querySelector(`.columna[data-key="${i}"]`);
+    
     var nuevaNota = document.createElement("div");
-    nuevaNota.setAttribute("class", "nota nota-" + i);
-    nuevaNota.dataset.velocidad = Math.random() * 10 + 1; // Asegurar que la velocidad no sea 0
+    nuevaNota.setAttribute("class", "nota");
+    nuevaNota.dataset.velocidad = (Math.random() * 10) + 1; // Asegurar que la velocidad no sea 0
     // Asignar un marginTop inicial para cada nota para evitar que se superpongan
     nuevaNota.style.marginTop = `-${i * 100}px`;
     nuevaNota.innerHTML = i;
-    contenedor.appendChild(nuevaNota);
+    contenedorColumna.appendChild(nuevaNota);
+}*/
+function crearNota(tecla) {
+    var contenedorColumna = document.getElementById(`columna${tecla}`);
+    if (contenedorColumna) {
+        var nuevaNota = document.createElement("div");
+        nuevaNota.classList.add("nota", `nota-${tecla}`);
+        // Agrega aquí cualquier otra configuración inicial para la nota
+        contenedorColumna.appendChild(nuevaNota);
+    } else {
+        console.error("No se encontró el contenedor de la columna para la tecla: " + tecla);
+    }
 }
+/*
+function crearNota(tecla) {
+    var contenedorColumna = document.getElementById(`columna${tecla}`);
+    if (contenedorColumna) {
+        var nuevaNota = document.createElement("div");
+        nuevaNota.setAttribute("class", "nota " + tecla);
+        nuevaNota.dataset.velocidad = (Math.random() * 10) + 1; // Asegurar que la velocidad no sea 0
+        // Asignar un marginTop inicial para cada nota para evitar que se superpongan
+        nuevaNota.style.marginTop = `-${tecla * 100}px`;
+        nuevaNota.innerHTML = tecla;
+        contenedorColumna.appendChild(nuevaNota);
+    } else {
+        console.error("No se encontró el contenedor de la columna para la tecla: " + tecla);
+    }
+}*/
 
 function moverNotas() {
     var notas = document.querySelectorAll(".nota");
@@ -25,33 +56,61 @@ function moverNotas() {
 
         // Verificar si la nota ha salido del contenedor
         if (nuevaPosicion > alturaContenedor) {
+            //nota.style.marginTop = `-50px`;
             nota.remove(); // Eliminar la nota del DOM
+        } else {
+            // Mover la nota hacia abajo según su velocidad
+            nota.style.marginTop = `${nuevaPosicion}px`;
         }
     });
 }
 
 function generarNotasContinuamente() {
     // Define un intervalo para la generación de notas
-    var intervaloDeNotas = setInterval(function() {
-        var i = Math.floor(Math.random() * 6) + 1; // Genera un número aleatorio entre 1 y 6 para el tipo de nota
-        crearNota(i);
-    }, 1000); // Crea una nota cada 1000 milisegundos (1 segundo)
+    setInterval(function () {
+        var notaAleatoria = teclas[Math.floor(Math.random() * teclas.length)];
+        crearNota(notaAleatoria);
+    }, 500); // Crea una nota cada 1000 milisegundos (1 segundo)
 
     // Para detener la generación de notas, usa clearInterval(intervaloDeNotas);
     // Podrías llamar a clearInterval cuando el juego se pause o termine
 }
 
-function start() {
-    // Limpiar el contenido previo para reiniciar el juego
-    contenedor.innerHTML = '';
-    pausado = false;
-    // Crear notas iniciales
-    for (i = 1; i <= 6; i++) {
-        crearNota(i);
-    }
-    intervalo = setInterval(moverNotas, 10);
-    generarNotasContinuamente(); 
+function manejarPresionDeTecla(teclaPresionada) {
+    console.log(teclaPresionada);
+    var notas = document.querySelectorAll(`.nota[data-key="${teclaPresionada}"]`);
+    notas.forEach(nota => {
+        console.log("entramos");
+        var posicionNota = parseFloat(nota.style.marginTop.replace('px', ''));
+        if (posicionNota > alturaContenedor - 100 && posicionNota < alturaContenedor - 50) {
+            // "zona de acierto"
+            console.log(`Acierto! Tecla ${teclaPresionada}`);
+            nota.remove(); // Eliminar la nota para indicar que fue "tocada"
+            increaseScore();
+            increaseStreak();
+        } else {
+            // Este es un error
+            resetStreak();
+        }
+    });
 }
+
+function start() {
+    pausado = false;
+
+    // Limpiar notas existentes antes de crear las iniciales
+    document.querySelectorAll('.columna.nota').forEach(nota => nota.remove());
+
+    // Crear una nota inicial para cada tecla en el arreglo `teclas`
+    teclas.forEach(crearNota);
+
+    // Configurar el intervalo para mover las notas
+    intervalo = setInterval(moverNotas, 10);
+
+    // Comenzar a generar notas continuamente
+    generarNotasContinuamente();
+}
+
 
 
 
@@ -75,6 +134,30 @@ function pausa() {
         togglePause();
     }
 }
+
+function reiniciarJuego() {
+    // Limpiar el contenedor de notas
+    document.querySelectorAll('.columna .nota').forEach(nota => nota.remove());
+
+    // Restablecer puntuación y cualquier otro estado relevante
+    document.querySelector('.score').textContent = 'Puntuación: 0';
+    document.querySelector('.streak').textContent = 'Racha: 0';
+    document.querySelector('.max-streak').textContent = 'Racha Máxima: 0';
+
+    // Detener cualquier música o sonido si es necesario
+    // let cancion = document.getElementById('song');
+    // cancion.pause();
+    // cancion.currentTime = 0; // Reiniciar el tiempo de la canción
+
+    // Detener la generación de notas y cualquier movimiento
+    if (typeof intervalo !== 'undefined') {
+        clearInterval(intervalo);
+    }
+
+    // Reiniciar el juego (esto podría incluir iniciar la música nuevamente y comenzar a generar notas)
+    start(); // Asumiendo que `start` es tu función para comenzar el juego
+}
+
 /*
 // Seguimiento de las teclas
 document.addEventListener("keyup", function (evt) {
@@ -99,28 +182,18 @@ document.addEventListener("keyup", function (evt) {
     }
 })*/
 document.addEventListener("keyup", function (evt) {
-    console.log(evt.key)
-    var hitZonePosition = alturaContenedor - 50; // for example, 50px from the bottom of the container
-    var notes = document.querySelectorAll(".nota");
-
-    notes.forEach(function (nota) {
-        var notePosition = parseFloat(nota.style.marginTop.replace('px', ''));
-        // Check if the note is within the hit zone
-        if (notePosition > hitZonePosition - 10 && notePosition < hitZonePosition + 10) {
-            if (evt.key === nota.getAttribute('data-key')) {
-                // This is a hit
-                nota.remove(); // Remove the note from the DOM
-                increaseScore();
-                increaseStreak();
-            } else {
-                // This is a miss
-                resetStreak();
-            }
-        }
-    });
-
-    if (evt.key === "q") {
+    var key = evt.key.toUpperCase();
+    if (key === "Q") {
         start();
+    }
+    if (['A', 'S', 'D', 'J', 'K', 'L'].includes(key)) {
+        manejarPresionDeTecla(evt.key);
+    }
+    if (key === "R") {
+        reiniciarJuego();
+    }
+    if (key === "P") {
+        pausa();
     }
 });
 
